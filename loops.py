6923db5a758d563e3442a95f6f5f55979c8ccc67
mohit9003 <mohitkumar900680@gmail.com>
2026-02-07 01:06:41 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,67 @@
+# """for i in range(5):
+#     print(i)
+
+# for i in range(2,6):
+#     print(i)
+
+
+# count = 0
+# while count<5:
+#     print(count)
+#     print += 1"""
+
+
+
+# for i in range(1,11):
+#     print(i)
+
+# i = 1
+# sum = 0
+
+# while i <= 10:
+#     sum += i
+#     i += 1
+
+# print("Sum of first 10 natural numbers =", sum)
+
+
+# num = int(input("enter a num"))
+
+# for i in range (11):
+#     i += 1
+#     print(sum*i)
+
+# num = int(input("Enter Factorial"))
+# fact = 1
+# i = 1
+# while i <= num:
+#       fact *= i
+#       i += 1
+# print(fact)
+
+
+# num = int(input("Enter number"))
+
+# rev = 0
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num = num//10
+# print(rev)
+
+# for i in range(0 , 51 , 2):
+#     print(i)
+   
+# num = int(input("enter number --"))
+
+# sum = 0
+
+# while num > 0:
+#     digit = num % 10
+#     sum += digit
+#     num = num // 10
+# print(sum)
+
+# n = int(input("enter no of row"))
+# for i in range (1, n + 1):
+#     print("*" *i)
