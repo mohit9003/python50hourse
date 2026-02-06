d0cb55139f55eef2ad21dc417c16c25823687d73
mohit9003 <mohitkumar900680@gmail.com>
2026-02-06 15:28:23 +0530

    Add files via upload

---
+++ conditionalstatement.py
@@ -0,0 +1,166 @@
+"""num = int(input("Enter a number"))
+if num > 0:
+    print("positive number")
+
+    if num % 2 == 0:
+       print("even")
+
+    else:
+        print("odd")
+
+else:
+    print("not a positive number")"""
+
+
+
+
+"""age = int(input("enter a number"))
+if age > 18:
+    print("vote")
+else:
+    print("not eligible")"""
+
+
+
+"""num = int(input("enter a number"))
+if num % 2 == 0:
+       print("even")
+
+else:
+        print("odd")"""
+
+
+
+
+"""num1 = int(input("enter 1st number --"))
+num2 = int(input("enter 2nd number"))
+
+if num1 > num2:
+    print("num1")
+
+else:
+    print("num2")"""
+
+
+
+
+"""num = int(input("enter a number"))
+
+if num % 5 == 0:
+    print("divisible")
+
+else:
+    print("not divisible")"""
+
+
+
+"""marks = int(input("Enter marks --"))
+
+if marks >= 90:
+    print("A")
+
+elif marks >= 75:
+    print("B")
+
+elif marks >= 50:
+    print("c")
+
+else:
+    print("fail")"""
+
+
+"""num = int(input("Enter year--"))
+
+if num % 4 == 0:
+    print("leap year")
+
+else:
+    print("Not leap")"""
+
+
+
+"""num = int(input("Enter number"))
+
+if num < 10:
+    print("one digit")
+
+elif num > 10 and num < 100:
+    print("two digit")
+
+else:
+    print("three digit")"""
+
+
+"""num = int(input("Enter number"))
+
+if num == 1:
+    print("Sunday")
+
+elif num == 2:
+    print("Monday")
+
+elif num == 3:
+    print("tuesday")
+
+elif num == 4:
+    print("wednessday")
+
+elif num == 5:
+    print("thrusday")
+
+elif num == 6:
+    print("friday")
+
+elif num == 7:
+    print("saturday")
+
+else:
+    print("not")"""
+
+
+
+"""temp = int(input("enter degree--"))
+
+if temp < 0:
+    print("freezing")
+elif temp < 0: and temp < 20:
+    print("cold")
+
+elif temp > 21 and temp < 35:
+    print("warm")
+
+else:
+    print("hot")"""
+
+
+"""correct_password = "admin123"
+
+password = input("Enter password: ")
+
+if password == correct_password:
+    print("Access")
+else:
+    print("Not Access")"""
+
+
+
+"""side1 = int(input("ender 1--"))
+side2 = int(input("enter 2--"))
+side3 = int(input("enter 3--"))
+
+if side1+side2 > side3 or side2+side3 > side1 or side3+side1 > side2:
+     print("valid")
+
+else:
+     print("not valid")"""
+
+
+"""marks1 = int(input("physics"))
+marks2 = int(input("math"))
+marks3 = int(input("bio"))
+
+if marks1 >= 35 and marks2 >=35 and marks3 >= 35:
+    print("pass")
+else:
+    print("fail")
+"""
