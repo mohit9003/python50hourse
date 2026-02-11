f19906e01ff6713121cf967e44487cc51d7d4cb2
mohit9003 <mohitkumar900680@gmail.com>
2026-02-11 22:34:02 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,63 @@
+"""x=int(input("enter your first number-"))
+y=int(input("enter your second number-"))
+sum=x+y
+product=x*y
+quotient=x/y
+print(sum,product,quotient)"""
+
+
+"""a=15
+b=40
+
+total= 3*a + 2*b
+print(total)"""
+
+"""a = int(input("enter the number"))
+b = int(input("enter the number"))
+c = int(input("enter the number"))
+d = int(input("enter the number"))
+e = int(input("enter the number"))
+
+avg = (a+b+c+d+e)/5
+print(avg)"""
+
+
+"""x = int(input("enter number -"))
+squ = no*n0
+cube = no*no*no
+root = no*0.5
+print(squ,cube,root)
+"""
+
+
+"""second = int(input("enter second -"))
+min =second//60
+sec =second%60
+print(min,sec)"""
+
+"""distance = 120
+time = 3
+speed = distance/time
+print(speed)"""
+
+"""x =int(input("enter first number"))
+y =int(input("enter second number"))
+
+if x%y==0:
+    print("yes")
+else:
+    print("no")"""
+
+
+"""p = int(input("enter p-"))
+r = int(input("enter r-"))
+t = int(input("enter t -"))
+
+si = (p*r*t)/100
+print(si)"""
+
+x = float(input("enter first number"))
+y = float(input("enter second number"))
+
+qoutient = x//y
+print(qoutient)
