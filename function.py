9b1ecdd89fd925827c5444445d0fb3f4c247c8a5
mohit9003 <mohitkumar900680@gmail.com>
2026-02-11 22:31:33 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,90 @@
+# def hii():
+#     print("hello world")
+
+# hii()
+
+
+# def hii(mohit):
+#     print("hello",mohit)
+
+# hii("mohit")
+
+
+# def add(a, b):
+#     result = 5 + 2
+#     return result
+# print(add)
+
+
+# def mult(a , b):
+#     result = 5 * 2
+#     return result
+# print(mult)
+
+
+# def even_odd(number):
+#     if number % 2 == 0:
+#       return "even"
+#     else:
+#       return "odd"
+# num1 = 3
+# print(even_odd(num1))
+# num2 = 7
+# print(even_odd(num2))
+
+
+# def num_avg(numbers):
+#     total = 0
+#     for num in numbers:
+#         total += num
+#     avg = total/5
+#     return avg
+
+# average = num_avg([2,3,6,8,2])
+# print(average)
+
+
+# def max(a, b):
+#     if a > b:
+#         return "max"
+#     else:
+#         return "no"
+    
+# a = 5
+# b = 3
+
+# print(max(a , b))
+
+# def count_vowels(s):
+#     count = 0
+#     vowels = "aeiouAEIOU"
+    
+#     for ch in s:
+#         if ch in vowels:
+#             count += 1
+    
+#     return count
+# text = "hii my name is mohit"
+# print(count_vowels(text))
+
+
+# def hi_palindrome(s):
+#     s = s.lower()
+#     return s == s [::-1]
+
+# text = "madam"
+# if hi_palindrome(text):
+#     print("palindrome")
+
+# else:
+#     print("not palindrome")
+
+
+# def intrest(p,r,t):
+#     si = (p*r*t)/100
+#     return si
+# p = 100000000
+# r = 5
+# t = 10
+# print("nishat dede")
+# print(intrest(p,r,t))
