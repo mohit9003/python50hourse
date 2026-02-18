22c220106c08435da6b293869375745e2262caac
mohit9003 <mohitkumar900680@gmail.com>
2026-02-18 22:01:55 +0530

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,102 @@
+import mymath
+
+# print ("addition", mymath.add(10 , 5))
+# print ("subtract", mymath.subtract(10, 5))
+
+# import math
+# a = 6
+# b = math.factorial(a)
+# print(b)
+
+
+# import math
+# a = 4.7
+# b = math.ceil(a)
+# print(b)
+
+
+# import math
+# a = 5
+# b = math.pow(a,3)
+# print(b)
+
+# import math
+
+
+# a = -18
+# b = math.fabs(a)
+# print(b)
+
+
+# a = math.exp(3)
+# print(a)
+
+
+# a = math.log10(1000)
+# print(a)
+
+# degree = 180
+# a = math.radians(degree)
+# print(a)
+
+
+import random
+
+# a = random.randint(1, 10)
+# print(a)
+
+
+# a = random.random()
+# print(a)
+
+
+# a = [10,20,30,40,50]
+# b = random.choice(a)
+# print(b)
+
+
+# a = [1,2,3,4,5]
+# b = random.shuffle(a)
+# print(b)
+
+# a = random.randrange(2,21,2)
+# print(a)
+
+
+# for i in range(5):
+#     num = random.randint(10,100)
+#     print(num)
+
+
+# a = random.randint(1, 6)
+# print(a)
+
+# import num_tools
+
+# print(num_tools.square(10))
+# print(num_tools.cube(2))
+
+
+
+# import temp_converter
+
+# c = 25
+# print("Fahrenheit", temp_converter.c_to_f(c))
+
+# f = 99
+# print("Celsius", temp_converter.f_to_c(f))
+
+
+
+
+# import area_module
+
+# r = 7
+# length = 10
+# width = 4
+# base = 6
+# height = 8
+
+# print(area_module.area_circle(r))
+# print(area_module.area_rectangle(length, width))
+# print(area_module.area_triangle(base * height))
---
+++ modules/temp_converter.py
@@ -0,0 +1,8 @@
+def c_to_f(c):
+    f = (c * 9/5) + 32
+    return f
+
+
+def f_to_c(f):
+    c = (f - 32) * 5/9
+    return c
